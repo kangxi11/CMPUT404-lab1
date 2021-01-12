57dcc91b52cb48f33837fe381a0b3cb0338ba8ec
Ryan Kang <rkang2009@gmail.com>
2021-01-12 13:44:45 -0500

    requests raw file from github

--- version.py
+++ version.py
@@ -1,4 +1,9 @@
 import requests
 
-print(requests.__version__)
-print(requests.get("https://www.google.com/"))
+url = "https://raw.githubusercontent.com/kangxi11/CMPUT404-lab1/main/version.py"
+# print(requests.__version__)
+# print(requests.get("https://www.google.com/"))
+
+r = requests.get(url)
+
+print(r.text)
